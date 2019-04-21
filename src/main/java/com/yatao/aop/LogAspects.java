08f0c9434f9e7d686d12f2474c9183754d33660b
Evan <xuyt1992@163.com>
2019-04-21 17:12:33 +0800

    add ：scope 注解

--- src/main/java/com/yatao/aop/LogAspects.java
+++ src/main/java/com/yatao/aop/LogAspects.java
@@ -22,7 +22,7 @@ public class LogAspects {
     //抽取公共的切入点表达式
     //1、本类引用
     //2、其他的切面引用
-    @Pointcut("execution(public int com.atguigu.aop.MathCalculator.*(..))")
+    @Pointcut("execution(public int com.yatao.aop.MathCalculator.*(..))")
     public void pointCut() {
     }
 
@@ -35,7 +35,7 @@ public class LogAspects {
         System.out.println("" + joinPoint.getSignature().getName() + "运行。。。@Before:参数列表是：{" + Arrays.asList(args) + "}");
     }
 
-    @After("com.atguigu.aop.LogAspects.pointCut()")
+    @After("com.yatao.aop.LogAspects.pointCut()")
     public void logEnd(JoinPoint joinPoint) {
         System.out.println("" + joinPoint.getSignature().getName() + "结束。。。@After");
     }
